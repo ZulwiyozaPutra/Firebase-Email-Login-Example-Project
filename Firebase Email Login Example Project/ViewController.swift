7d239b1d50fb5e13144126cb121897f979160df1
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2017-02-01 17:52:41 +0700

    Complete version

--- Firebase Email Login Example Project/ViewController.swift
+++ Firebase Email Login Example Project/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var emailLabel: UITextField!
+    
+    @IBOutlet weak var passwordLabel: UITextField!
+    
+    @IBAction func signInButton(_ sender: Any) {
+        
+        if let email = emailLabel.text, let password = passwordLabel.text{
+            FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                
+                if error != nil {
+                    print("There was an error occured \(error.debugDescription)")
+                    self.signIn()
+                    
+                } else {
+                    print("\(user.debugDescription) user created")
+                    self.signIn()
+                }
+                
+            })
+        }
+        
+        
+    }
+    
+    private func signIn() -> Void {
+        if let email = emailLabel.text, let password = passwordLabel.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                if error != nil {
+                    print("There was an error occured \(error.debugDescription)")
+                } else {
+                    print("Logged in")
+                }
+            })
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
